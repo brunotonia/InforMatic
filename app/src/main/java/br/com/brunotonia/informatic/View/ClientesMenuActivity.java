6b4e7264b275a53341538d4bcc0f31cd844c30e3
brunotonia <bruno.rvt@gmail.com>
2016-06-10 21:26:22 -0300

    Initial commit

---
+++ app/src/main/java/br/com/brunotonia/informatic/View/ClientesMenuActivity.java
@@ -0,0 +1,13 @@
+package br.com.brunotonia.informatic.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClientesMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clientes_menu);
+    }
+}
