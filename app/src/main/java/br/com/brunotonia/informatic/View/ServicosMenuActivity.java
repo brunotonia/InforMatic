f0b6a743b607b193abb5e61d1a79c0d99e133f16
brunotonia <bruno.rvt@gmail.com>
2016-06-12 15:16:26 -0300

    Versão 0.89a  - Clientes: adicionando, editando e listando.  - Serviços: gerando telas

--- app/src/main/java/br/com/brunotonia/informatic/View/ServicosMenuActivity.java
+++ app/src/main/java/br/com/brunotonia/informatic/View/ServicosMenuActivity.java
@@ -1,15 +1,63 @@
 package br.com.brunotonia.informatic.View;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import br.com.brunotonia.informatic.R;
 
 public class ServicosMenuActivity extends AppCompatActivity {
 
+    /* Variáveis entre telas */
+    private Intent it = null;
+    private Bundle params = null;
+
+    /* Declaração de elementos de interface */
+    Button btnAdicionar = null;
+    Button btnEditar    = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicos_menu);
+
+        /* Inicialização de elementos de interface */
+        btnAdicionar = (Button) findViewById(R.id.btnAdicionar);
+        btnEditar = (Button) findViewById(R.id.btnEditar);
+
+        /* Listeners dos botões */
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chamarTelaClientesAdd();
+            }
+        });
+
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chamarTelaClientesListar();
+            }
+        });
+
+    }
+
+    /* Métodos para chamar novas telas */
+    private void chamarTelaClientesAdd () {
+        params = new Bundle();
+        params.putLong("servicoID", -1L);
+        it = new Intent(this, ClientesAddActivity.class);
+        it.putExtras(params);
+        startActivity(it);
+    }
+
+    private void chamarTelaClientesListar () {
+        params = new Bundle();
+        params.putLong("servicoID", -2L);
+        it = new Intent(this, ClientesListarActivity.class);
+        it.putExtras(params);
+        startActivity(it);
     }
 }
